1413be428e407080ba7be79a80bcc6435f7ba612
Sameer Al Harbi <smrhrbi@gmail.com>
2018-10-05 15:50:13 +0300

    add edit news

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -11,11 +11,13 @@ import { ContactsComponent } from './contacts/contacts.component';
 import { NewsListComponent } from './news-list/news-list.component';
 import { NewsListItemComponent } from './news-list/news-list-item/news-list-item.component';
 import { NewsDetailsComponent } from './news-list/news-details/news-details.component';
+import { EditNewsComponent } from './news-list/edit-news/edit-news.component';
 
 const appRoutes: Routes = [
   { path: '', component: HomeComponent},
   { path: 'news', component: NewsListComponent},
   { path: 'news/:id/:title', component: NewsDetailsComponent},
+  { path: 'news/:id/:title/edit', component: EditNewsComponent},
   { path: 'about', component: AboutComponent},
   { path: 'contacts', component: ContactsComponent}
 ];
@@ -30,7 +32,8 @@ const appRoutes: Routes = [
     ContactsComponent,
     NewsListComponent,
     NewsListItemComponent,
-    NewsDetailsComponent
+    NewsDetailsComponent,
+    EditNewsComponent
   ],
   imports: [
     BrowserModule,
--- src/app/news-list/news-details/news-details.component.ts
+++ src/app/news-list/news-details/news-details.component.ts
@@ -1,22 +1,34 @@
-import { Component, OnInit } from '@angular/core';
-import { ActivatedRoute } from '@angular/router';
+import { Component, OnInit, OnDestroy } from '@angular/core';
+import { ActivatedRoute, Params } from '@angular/router';
+import { Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-news-details',
   templateUrl: './news-details.component.html',
   styleUrls: ['./news-details.component.css']
 })
-export class NewsDetailsComponent implements OnInit {
+export class NewsDetailsComponent implements OnInit, OnDestroy {
 
-  news: {id:number, title: string};
+  news: {id: number, title: string};
+  paramsSubscription: Subscription;
 
-  constructor(private route:ActivatedRoute) { }
+  constructor(private route: ActivatedRoute) { }
 
   ngOnInit() {
     this.news = {
       id: this.route.snapshot.params['id'],
-      title:this.route.snapshot.params['title']
+      title: this.route.snapshot.params['title']
     };
+    this.paramsSubscription = this.route.params
+      .subscribe((params: Params) => {
+        this.news.id = params['id'];
+        this.news.title = params['title'];
+      });
   }
+
+  ngOnDestroy(): void {
+    this.paramsSubscription.unsubscribe();
+  }
+
 
 }
